823bcf4fb93e294b16869ab6184b5163ac9347eb
Krishna Subramanian <krishi.70518@gmail.com>
2019-08-27 18:55:24 -0500

    Full Chatbot script

--- chatbot-2.0.py
+++ chatbot-2.0.py
@@ -714,3 +714,12 @@ trainIters(model_name, voc, pairs, encoder, decoder, encoder_optimizer, decoder_
            embedding, encoder_n_layers, decoder_n_layers, save_dir, n_iteration, batch_size,
            print_every, save_every, clip, corpus_name, loadFilename)
 
+# Set dropout layers to eval mode
+encoder.eval()
+decoder.eval()
+
+# Initialize search module
+searcher = GreedySearchDecoder(encoder, decoder)
+
+# Begin chatting (uncomment and run the following line to begin)
+# evaluateInput(encoder, decoder, searcher, voc)
